beec430575afeeffd93eacc03cb9dacd50eaeaa0
Ivan Camilo Rosales Rodriguez <rivancamilo@gmail.com>
2023-10-04 23:52:12 +0000

    en este punto creamos la tabla de dim_producto e insertamos datos

--- scripts/dim_producto.sql
+++ scripts/dim_producto.sql
@@ -1,47 +1,53 @@
-
+INSERT INTO datawh.Dim_Producto(
+	ProductID,
+	nomProducto,
+	categoria,
+	subcategoria,
+	tipoProceso,
+	clase,
+	modelo,
+	linea,
+	genero,
+	color,
+	estadoBodega
+)
 SELECT
 p.ProductID
-,p.Name as nomProducto
-,pc.Name as categoria
-,ps.Name as subCategoria
+,p.Name-- as nomProducto
+,pc.Name-- as categoria
+,ps.Name-- as subCategoria
 ,(case 
 	when p.MakeFlag = 1 then 'Comprado' 
-	else 'Fabricado' end) as tipoProceso
+	else 'Fabricado' end)-- as tipoProceso
 ,(case 
 	when p.Class = 'H' then 'Alta'
 	when p.Class = 'M' then 'Media'
 	when p.Class = 'L' then 'Baja'
 	else 'Sin Definir' end) as clase
-,pm.Name as modelo
+,pm.Name-- as modelo
 ,(case 
 	when p.ProductLine = 'R' then 'Carretera'
 	when p.ProductLine = 'S' then 'Estándar'
 	when p.ProductLine = 'M' then 'Montaña'
 	when p.ProductLine = 'T' then 'Paseo'
-	else 'Sin Clasificación' end ) as linea
+	else 'Sin Clasificación' end )-- as linea
 ,(case 
 	when p.Style = 'W' then 'Femenino'
 	when p.Style = 'M' then 'Maculino'	
-	else 'Unixes' end) as Genero
+	else 'Unixes' end)-- as Genero
 ,(case 
 	when p.Color is null then 'Sin Definir'
-	else p.Color end) as Color
+	else p.Color end)-- as Color
 ,(case 
 	when p.SafetyStockLevel < p.ReorderPoint then 'Poco Sotck'
-	else 'Buen Stock' end ) as estadoBodega
-FROM Production_Product p
-INNER JOIN Production_ProductModel pm
+	else 'Buen Stock' end )-- as estadoBodega
+FROM adw.Production_Product p
+LEFT JOIN adw.Production_ProductModel pm
 	ON p.ProductModelID = pm.ProductModelID
-INNER JOIN Production_ProductSubcategory ps
+LEFT JOIN adw.Production_ProductSubcategory ps
 	ON p.ProductSubcategoryID = ps.ProductSubcategoryID
-INNER JOIN Production_ProductCategory pc
-	ON ps.ProductCategoryID = pc.ProductCategoryID
-limit 3;
+LEFT JOIN adw.Production_ProductCategory pc
+	ON ps.ProductCategoryID = pc.ProductCategoryID;
 
-
-
-/*
-SELECT distinct ReorderPoint from Production_Product;
-*/
 
 
